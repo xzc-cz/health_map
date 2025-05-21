606dc588b1cacda0b6736dce4ed96412079e44e3
Xinjia Wang <wangxinjia98@gmail.com>
2025-05-21 22:24:19 +1000

    Login Screen， Register Screen，Reset Screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.google.services) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 
